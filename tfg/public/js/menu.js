439158e8bc01864a0a52cff0f3b5cae0fe407c6f
Javigl <jgl83@alu.ua.es>
2022-06-19 17:00:33 +0200

    Cambios

--- tfg/public/js/menu.js
+++ tfg/public/js/menu.js
@@ -4,19 +4,10 @@
     const closeMenu = document.querySelector('.nav__close');
 
     openButton.addEventListener('click', ()=>{
-<<<<<<< HEAD
         menu.classList.add('nav__link--show')
     })
-=======
-        menu.classList.add('nav__link--show');
-    });
->>>>>>> develop
 
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav__link--show');
     })
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 })();
